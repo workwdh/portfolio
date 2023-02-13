ac831f451760c35a9a41513949f8d13ab17f7632
workwdh <98251011+workwdh@users.noreply.github.com>
2023-02-13 23:40:38 +0900

    JS: Home Contact Button

--- main.js
+++ main.js
@@ -14,15 +14,21 @@ document.addEventListener('scroll', () => {
 // navbar scroll to id
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
         return;       
     }
-
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+    scrollIntoView(link);
 });
 
+// handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+};
