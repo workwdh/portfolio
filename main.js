d902f4d85eb3977ba4210fb0731b5e936627b559
workwdh <98251011+workwdh@users.noreply.github.com>
2023-02-28 18:47:10 +0900

    JS: Navbar menu toggle+ project UI tweak

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -18,7 +19,14 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+navbarMenu.classList.toggle('open');
 });
 
 // handle click on "contact me" button on home
